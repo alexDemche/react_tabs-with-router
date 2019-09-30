744028d109b3708d6ede0bc4a70ccdbd02a4dd88
alexDemche <alexeyydemchenko@gmail.com>
2019-09-30 14:23:14 +0300

    Updates

--- precache-manifest.8a0d0e4b7a804e000b2746b9f1380c7a.js
+++ precache-manifest.8a0d0e4b7a804e000b2746b9f1380c7a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3fd047c9cf3b327bdd40dde9c64ff1a6",
+    "revision": "239b60169846f41cadf74022ad4f250f",
     "url": "/react_tabs-with-router/index.html"
   },
   {
-    "revision": "2a21971bc7cdc5b9ba5d",
+    "revision": "1ba01d024727c5970aa0",
     "url": "/react_tabs-with-router/static/css/main.0436ef1e.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react_tabs-with-router/static/js/2.d1f50204.chunk.js"
   },
   {
-    "revision": "2a21971bc7cdc5b9ba5d",
-    "url": "/react_tabs-with-router/static/js/main.ba6e3eb3.chunk.js"
+    "revision": "1ba01d024727c5970aa0",
+    "url": "/react_tabs-with-router/static/js/main.64cbe88b.chunk.js"
   },
   {
     "revision": "da3ca6fceb301548d22d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_tabs-with-router/precache-manifest.c783dc715d42aa14db5fd6b86a380704.js"
+  "/react_tabs-with-router/precache-manifest.8a0d0e4b7a804e000b2746b9f1380c7a.js"
 );
 
 self.addEventListener('message', (event) => {
