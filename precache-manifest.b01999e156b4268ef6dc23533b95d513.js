3981e3837eb9af7ffd0c6693fca43c5abf0bc6ff
alexDemche <alexeyydemchenko@gmail.com>
2019-09-30 14:01:00 +0300

    Updates

---
+++ precache-manifest.b01999e156b4268ef6dc23533b95d513.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "607751c3311cb5dbc11a83744df9db66",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "6d5827783b70618502cc",
+    "url": "/react_tabs-with-router/static/css/main.0436ef1e.chunk.css"
+  },
+  {
+    "revision": "7a3ccb7d0caf0c31a5a1",
+    "url": "/react_tabs-with-router/static/js/2.d1f50204.chunk.js"
+  },
+  {
+    "revision": "6d5827783b70618502cc",
+    "url": "/react_tabs-with-router/static/js/main.b494aad6.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
